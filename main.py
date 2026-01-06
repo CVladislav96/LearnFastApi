54375acaa5cd77a5ac3becb4a3b93367069d1049
Vladislav <email@gmail.com>
2026-01-06 19:35:00 +0200

    learn

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+    from fastapi import FastAPI
 from fastapi.responses import FileResponse
 
 app = FastAPI(
@@ -8,6 +8,34 @@ app = FastAPI(
 )
 
 
+fake_tasks_db = [
+    {"task_id": 1, "task_name": "Изучить Python"},
+    {"task_id": 2, "task_name": "Подключить Базу Данных"},
+    {"task_id": 3, "task_name": "Выучить FastAPI"},
+]
+
+
 @app.get("/")
 async def root():
     return FileResponse("index.html")
+
+@app.get("/tasks/{task_id}")
+async def get_task(task_id: int):
+    for task in fake_tasks_db:
+        if task["task_id"] == task_id:
+            return task
+    return {}
+
+@app.get("/hello/{name}")
+async def hello_user(name: str):
+         return {"message": f"Hello, {name}!"}
+
+
+@app.get("/product/{product_id}")
+async def search_product(product_id: int):
+    return {"product_id": product_id}
+
+@app.get("/flights/{from_code}/{to_code}")
+async def direct(from_code: int, to_code: int):
+    return {"from": from_code, "to": to_code}
+
