62ec27f9d94a819a6917ac35a92cc119dd70d8d9
Vladislav <email@gmail.com>
2026-01-06 16:05:23 +0200

    podpravil html

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
 from fastapi.responses import FileResponse
 
-app = FastAPI()
+app = FastAPI(
+    title="Task Manager API",
+    description="Учебное приложение для курса по FastAPI",
+    version="1.0.0"
+)
 
 
 @app.get("/")
